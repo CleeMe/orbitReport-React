a9487669c12d0da2270ab61bb4dc84b96d32d32a
Christi-Lee <133257586+CleeMe@users.noreply.github.com>
2023-07-20 12:15:29 -0500

    added in orbit/filter and props for Buttons and Table components

---
+++ src/components/Banner.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div> 
+    <header>Orbit Report</header>
+    <p>"Click on the buttons to see the satellites in that orbit type"</p>
+    </div>
+  );
+
+};
+
+export default Banner;
